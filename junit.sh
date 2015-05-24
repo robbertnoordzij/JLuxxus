2661f9f8f81d309b36a69213607f7e6292242b54
Robbert Noordzij <github@robbertnoordzij.nl>
2015-05-24 13:24:58 +0200

    CI

---
+++ junit.sh
@@ -0,0 +1,13 @@
+# Download libs
+wget http://search.maven.org/remotecontent?filepath=org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar -O hamcrest.jar
+wget http://search.maven.org/remotecontent?filepath=junit/junit/4.12/junit-4.12.jar -O junit.jar
+
+# Compile sources and runt tests
+find . -type f -name "*.java" > sources.txt
+javac -cp junit.jar hamcrest.jar @sources.txt 
+java -cp junit.jar:hamcrest.jar:./bin/ org.junit.runner.JUnitCore AllTests
+
+# Clean up
+rm hamcrest.jar
+rm junit.jar
+rm sources.txt
