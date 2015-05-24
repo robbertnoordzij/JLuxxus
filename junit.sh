b3ad443cf45ea3360a080b4992c9a8ce6e0301cf
Robbert Noordzij <github@robbertnoordzij.nl>
2015-05-24 13:27:21 +0200

    Set correct directory

--- junit.sh
+++ junit.sh
@@ -3,8 +3,9 @@ wget http://search.maven.org/remotecontent?filepath=org/hamcrest/hamcrest-core/1
 wget http://search.maven.org/remotecontent?filepath=junit/junit/4.12/junit-4.12.jar -O junit.jar
 
 # Compile sources and runt tests
+mkdir -p bin
 find . -type f -name "*.java" > sources.txt
-javac -cp junit.jar hamcrest.jar @sources.txt 
+javac -d ./bin/ -cp junit.jar hamcrest.jar @sources.txt
 java -cp junit.jar:hamcrest.jar:./bin/ org.junit.runner.JUnitCore AllTests
 
 # Clean up
