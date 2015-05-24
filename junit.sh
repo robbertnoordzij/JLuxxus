97638d0ed01c048ae01d735309616377bbdb94f8
Robbert Noordzij <github@robbertnoordzij.nl>
2015-05-24 13:32:27 +0200

    Fix build path

--- junit.sh
+++ junit.sh
@@ -5,7 +5,7 @@ wget http://search.maven.org/remotecontent?filepath=junit/junit/4.12/junit-4.12.
 # Compile sources and runt tests
 mkdir -p bin
 find . -type f -name "*.java" > sources.txt
-javac -d ./bin/ -cp junit.jar hamcrest.jar @sources.txt
+javac -d ./bin/ -cp junit.jar @sources.txt
 java -cp junit.jar:hamcrest.jar:./bin/ org.junit.runner.JUnitCore AllTests
 
 # Clean up
