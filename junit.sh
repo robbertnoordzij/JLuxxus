f8f06cc5078d94afcd3fb3590a3ddb48adfd4cb7
Robbert Noordzij <github@robbertnoordzij.nl>
2015-05-24 13:44:34 +0200

    Use correct exit code

--- junit.sh
+++ junit.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Download libs
 wget http://search.maven.org/remotecontent?filepath=org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar -O hamcrest.jar
 wget http://search.maven.org/remotecontent?filepath=junit/junit/4.12/junit-4.12.jar -O junit.jar
@@ -6,9 +8,13 @@ wget http://search.maven.org/remotecontent?filepath=junit/junit/4.12/junit-4.12.
 mkdir -p bin
 find . -type f -name "*.java" > sources.txt
 javac -d ./bin/ -cp junit.jar @sources.txt
-java -cp junit.jar:hamcrest.jar:./bin/ org.junit.runner.JUnitCore AllTests
 
-# Clean up
+java -cp junit.jar:hamcrest.jar:./bin/ org.junit.runner.JUnitCore AllTests
+code=$?
+
+# Cleand up
 rm hamcrest.jar
 rm junit.jar
 rm sources.txt
+
+exit $code
